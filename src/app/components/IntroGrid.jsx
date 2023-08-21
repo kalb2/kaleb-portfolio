c01d78e088851664b01d1ce0c5bf7839287208ab
kalb2 <132865166+kalb2@users.noreply.github.com>
2023-08-21 14:05:03 -0600

    Removed Numbers

--- src/app/components/IntroGrid.jsx
+++ src/app/components/IntroGrid.jsx
@@ -7,9 +7,9 @@ export default function IntroGrid() {
         <h1 className="text-6xl uppercase tracking-widest">Welcome</h1>
       </div>
       <div className="grid grid-rows-2 grid-cols-3 h-80">
-        <div className="row-span-2 border-r border-b">1</div>
-        <div className="border-r border-b">2</div>
-        <div className="border-b">3</div>
+        <div className="row-span-2 border-r border-b"></div>
+        <div className="border-r border-b"></div>
+        <div className="border-b"></div>
         <div className="border-r border-b">
           <h2 className="text-5xl uppercase">
             A Front-end
@@ -17,7 +17,7 @@ export default function IntroGrid() {
             trade
           </h2>
         </div>
-        <div className=" border-b">5</div>
+        <div className=" border-b"></div>
       </div>
     </>
   );
