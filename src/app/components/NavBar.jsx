4faa3ab80cd64a71d3b3484d6e71ab687c7f038a
kalb2 <132865166+kalb2@users.noreply.github.com>
2023-08-21 16:15:59 -0600

    Added Linkedin Logo

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import githublogo from "../assets/github-mark.svg"
 import kjlogo from "../assets/KJ-Logo.svg"
+import linkedinlogo from "../assets/LI-in-Bug.png"
 
 export default function NavBar() {
   return (
@@ -12,12 +13,15 @@ export default function NavBar() {
       </Link>
     </div>
     <div className="border-r border-black p-0 h-full flex flex-1 items-center justify-end" >
+      <a href="https://www.linkedin.com/in/kalebjensen/" target="_blank">
+      <Image src={linkedinlogo} width={45} height={"auto"} alt='my LinkedIn' className='mr-5'/>
+      </a>
       <a href="https://github.com/kalb2" target="_blank">
-      <Image src={githublogo} width={45} height={"auto"} alt='github logo' className='mr-5'/>
+      <Image src={githublogo} width={45} height={"auto"} alt='my Github' className='mr-5'/>
       </a>
     </div>
     <div className="border-r border-black px-5 h-full flex items-center">
-      <Link href="/projects">Projects</Link>
+      <Link href="/projects" className=' active:underline'>Projects</Link>
     </div>
     <div className="border-r border-black px-5 h-full flex items-center">
       <Link href="/experience">Experience</Link>
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import CardRow from "./components/CardRow";
 import IntroGrid from "./components/IntroGrid";
+import TitleBar from "./components/TitleBar";
 
 export default function Home() {
   return (
 <>
 <IntroGrid />
+<TitleBar text="Recent Projects" />
 <CardRow />
 </>
   );
