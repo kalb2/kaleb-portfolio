501676795298bcea110a43447cf5fb39b94de7a4
kalb2 <132865166+kalb2@users.noreply.github.com>
2023-08-21 21:29:59 -0600

    Replace Icloud files

---
+++ src/app/components/CardHandles.jsx
@@ -0,0 +1,14 @@
+export default function CardHandles() {
+  return (
+    <>
+      <div className="handles"></div>
+      <div className="handles left-1/2"></div>
+      <div className="handles left-full"></div>
+      <div className="handles top-1/2"></div>
+      <div className="handles top-1/2 left-full"></div>
+      <div className="handles top-full"></div>
+      <div className="handles top-full left-1/2"></div>
+      <div className="handles top-full left-full"></div>
+    </>
+  );
+}
