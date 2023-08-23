399afa43acc1797db29b29ba2fdf3738e5ad5548
kalb2 <132865166+kalb2@users.noreply.github.com>
2023-08-23 09:42:13 -0600

    Addded Section Break

--- src/app/components/SectionBreak.jsx
+++ src/app/components/SectionBreak.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
 export default function SectionBreak() {
+  const sectionStyle = {
+    minHeight: 'calc(9vw + 3vh)',
+    lineHeight: '1',
+  };
+
   return (
-    <div className='w-full grid grid-cols-3 border-b h-[calc(9vw + 3vh)]'>
-      <div className='border-r' />
-      <div className='border-r' />
-      <div />
+    <div className='w-full grid grid-cols-3 border-b'>
+      <div className='border-r' style={sectionStyle} />
+      <div className='border-r' style={sectionStyle} />
+      <div style={sectionStyle} />
     </div>
   );
 }
+
+
+// import React from 'react';
+
+// export default function SectionBreak() {
+//   return (
+//     <div className='w-full grid grid-cols-3 border-b h-[calc(9vw + 3vh)]'>
+//       <div className='border-r' />
+//       <div className='border-r' />
+//       <div />
+//     </div>
+//   );
+// }
