93540e8d18ed8535dfb7ed288d4fe6932a4337d6
kalb2 <132865166+kalb2@users.noreply.github.com>
2023-09-11 10:09:19 -0600

    Updated About Me

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -12,14 +12,14 @@ export default function AboutPage() {
         leftTitle="Bio"
         rightTitle="Kaleb Jensen"
         bgimage="/Dubai.jpeg"
-        subtitle={`Husband.<br/> Dad.<br/> Dev.`}
-        paragraph1={`After graduating with a degree in Marketing and spending the next phase of my career in Marketing and Sales, I decided to make a transition to a career in Front-End Web Development.`}
-        paragraph2={`I have always had a passion for creating things, and found that
-          Front-End Development gave me an outlet to create limitless
-          projects.`}
-        paragraph3={` My approach to Front-End Development is the same as my approach to many things in life:`}
+        // subtitle={`Husband.<br/> Dad.<br/> Dev.`}
+        // subtitle={`Jack of all trades, master of some. `}
+        subtitle={`Husband. Dad. Dev. Traveler. Woodworker. Videographer. Lakers Fan. Golfer. `}
+        paragraph1={`With a wide variety of interests, there's always something to keep me busy. When I'm not coding, I'm spending time with my family or pursuing these interests.`}
+        paragraph2={`Thankfully I know how to teach myself how to do just about anything (with the help of Youtube). `}
+        paragraph3={`I wouldn't consider myself in expert in any of these things, which is why this is one of my favorite quotes:`}
         paragraph4={` "All I know is that I know nothing." - Socrates`}
-        // paragraph5={` My approach to Front-End Development is the same as my approach to many things in life:`}
+        paragraph5={`No matter how much I learn, there's just as much that I still don't. I try and embrace that and always pursue learning.`}
         // paragraph6={` "All I know is that I know nothing." - Socrates`}
         href1="https://www.linkedin.com/in/kalebjensen/"
         href1title="LinkedIn"
